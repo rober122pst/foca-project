e76c78f8367722551eb1d93c1023eeefba2b133e
rober122pst <jrobertinhor.9@gmail.com>
2025-11-07 20:31:53 -0300

    feat: Database configurado com prisma e supabase

--- server/server.js
+++ server/server.js
@@ -1,14 +1,40 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
     res.send('Server is running');
+});
+
+app.post('/users', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const newUser = await prisma.user.create({
+            data: { name, email, password }
+        });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create user' + error.message });
+    }
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch users: ' + error.message });
+    }
 });
 
 app.listen(PORT, () => {
