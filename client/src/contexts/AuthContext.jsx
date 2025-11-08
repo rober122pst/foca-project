4840cf903c75f2dad27d105642206c7203b1acbe
rober122pst <jrobertinhor.9@gmail.com>
2025-11-08 18:45:06 -0300

    feat: AuthProvider

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { getMe } from "../services/userService.js";
+import { loginUser, registerUser } from "../services/oauthService.js";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const res = await getMe();
+                setIsLoggedIn(true);
+                setUser(res.data);
+            } catch (error) {
+                setIsLoggedIn(false);
+                setUser(null);
+                console.error("Falha na auteticação:", error)
+            }finally {
+                setIsLoading(false);
+            }
+        };
+        checkAuth();
+    }, []);
+    
+    const login = async (data) => {
+        await loginUser(data);
+        setIsLoggedIn(true);
+    }
+    
+    const register = async (data) => {
+        await registerUser(data);
+    }
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, user, isLoading, login, register }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => { // novo hook (acho que se chama assim)
+    return useContext(AuthContext);
+}
--- server/src/auth/auth.middleware.js
+++ server/src/auth/auth.middleware.js
@@ -1,17 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 
-export default async function authMiddleware(req, res) {
-    const authHeader = req.headers.authorization;
+export default async function authMiddleware(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Token não fornecido' });
+      return res.status(401).json({ message: 'Token não fornecido' })
     }
 
-    const token = authHeader.split(' ')[1];
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.userId;
-        return req;
-    } catch (error) {
-        return res.status(401).json({ message: 'Token inválido' });
-    }
+    const token = authHeader.split(' ')[1]
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.userId = decoded.userId
+    next()
+  } catch (error) {
+    return res.status(401).json({ message: 'Token inválido' })
+  }
 }
