96988b40479d3bb3055ac4c5a5f0cf49d1a95a65
rober122pst <jrobertinhor.9@gmail.com>
2025-11-08 14:01:22 -0300

    feat: Fazendo a sidebar

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Dashboard() {
+    return (
+        <div className="flex">
+            <Sidebar />
+        </div>
+    );
+}
