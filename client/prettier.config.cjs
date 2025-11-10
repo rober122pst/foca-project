bd2eff887c98e9579a842bbd00c6547595ea2ba7
rober122pst <jrobertinhor.9@gmail.com>
2025-11-10 20:47:25 -0300

    Sidebar finalizada com botão pra expandir e navegação com SPA

--- client/prettier.config.cjs
+++ client/prettier.config.cjs
@@ -3,4 +3,6 @@ module.exports = {
     plugins: [import('prettier-plugin-tailwindcss')],
     jsxBracketSameLine: false,
     bracketSameLine: false,
+    // Force LF line endings to avoid CR (\r) insertion on Windows
+    endOfLine: 'lf',
 };
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -8,9 +8,11 @@ import { IoMenu } from 'react-icons/io5';
 
 import focaLogo from '../assets/logos/foca_logo_uncolor.svg';
 import focaLogoTypo from '../assets/logos/foca_logo_uncolor_typo.svg';
+import { useState } from 'react';
 
 export default function Sidebar() {
     const location = useLocation();
+    const [isOpen, setIsOpen] = useState(true);
 
     const menuItems = [
         { name: 'Overview', to: '/', icon: <FaChartLine /> },
@@ -27,23 +29,28 @@ export default function Sidebar() {
 
     return (
         <aside
+            data-open={isOpen}
             className={
-                'bg-cream-100 dark:bg-night-950 text-items-950 dark:text-cream-100 border-items-500 flex h-screen w-64 flex-col overflow-hidden border-r-5 border-double py-8'
+                'bg-cream-100 dark:bg-night-950 text-items-950 dark:text-cream-100 border-items-500 border-groove box-border flex h-screen w-64 flex-col overflow-hidden border-r-5 py-8 duration-700 data-[open=false]:w-22'
             }
         >
-            <img src={focaLogoTypo} alt="foca-logo" className="h-full max-h-10" />
+            <img src={isOpen ? focaLogoTypo : focaLogo} alt="foca-logo" className="h-full max-h-10" />
             <nav className="transition-theme my-8 mr-0 ml-4 space-y-4">
                 <ul>
                     {menuItems.map((item) => {
                         const isActive = '/' + location.pathname.split('/')[2] === item.to;
 
                         return (
-                            <Link to={`.${item.to}`} key={item.name}>
+                            <Link replace to={`.${item.to}`} key={item.name}>
                                 <li
-                                    className={`hover:shadow-items-500/50 transition-theme hover:text-cream-100 flex cursor-pointer items-center gap-4 rounded-l-lg p-4 text-xl hover:shadow-[inset_250px_0_0] ${isActive && 'shadow-items-500/50 shadow-[inset_250px_0_0]'}`}
+                                    className={`hover:shadow-items-500/50 transition-theme hover:text-cream-100 box-border flex h-15 cursor-pointer items-center gap-4 rounded-l-lg p-4 text-xl text-nowrap hover:shadow-[inset_250px_0_0] ${isActive && 'shadow-items-500/50 shadow-[inset_250px_0_0]'}`}
                                 >
-                                    {item.icon}
-                                    {item.name}
+                                    <span>{item.icon}</span>
+                                    <span
+                                        className={`transition-theme whitespace-nowrap delay-300 ${!isOpen && '-translate-x-3 opacity-0'}`}
+                                    >
+                                        {item.name}
+                                    </span>
                                 </li>
                             </Link>
                         );
@@ -54,21 +61,33 @@ export default function Sidebar() {
                 <ul>
                     {menuInsights.map((item) => (
                         <Link to={`.${item.to}`} key={item.name}>
-                            <li className="hover:shadow-items-500/50 transition-theme hover:text-cream-100 flex cursor-pointer items-center gap-4 rounded-l-lg p-4 text-xl hover:shadow-[inset_250px_0_0]">
-                                {item.icon}
-                                {item.name}
+                            <li className="hover:shadow-items-500/50 transition-theme hover:text-cream-100 box-border flex h-15 cursor-pointer items-center gap-4 rounded-l-lg p-4 text-xl text-nowrap hover:shadow-[inset_250px_0_0]">
+                                <span>{item.icon}</span>
+                                <span
+                                    className={`transition-theme whitespace-nowrap delay-300 ${!isOpen && '-translate-x-3 opacity-0'}`}
+                                >
+                                    {item.name}
+                                </span>
                             </li>
                         </Link>
                     ))}
                     <a href="/auth/logout">
-                        <li className="hover:shadow-items-500/50 transition-theme hover:text-cream-100 flex cursor-pointer items-center gap-4 rounded-l-lg p-4 text-xl hover:shadow-[inset_250px_0_0]">
-                            <MdLogout />
-                            Sair
+                        <li className="hover:shadow-items-500/50 transition-theme hover:text-cream-100 box-border flex h-15 cursor-pointer items-center gap-4 rounded-l-lg p-4 text-xl hover:shadow-[inset_250px_0_0]">
+                            <span>
+                                <MdLogout />
+                            </span>
+                            <span
+                                className={`transition-theme whitespace-nowrap delay-300 ${!isOpen && '-translate-x-3 opacity-0'}`}
+                            >
+                                Sair
+                            </span>
                         </li>
                     </a>
                 </ul>
             </nav>
-            <IoMenu size={30} className="text-items-500 mx-auto mt-auto mb-8 cursor-pointer" />
+            <button className="mx-auto mt-auto mb-8 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+                <IoMenu size={30} className="text-items-500" />
+            </button>
         </aside>
     );
 }
