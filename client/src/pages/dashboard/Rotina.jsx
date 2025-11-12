b579b99fee26e1a46992039c7a4c6231ce836706
rober122pst <jrobertinhor.9@gmail.com>
2025-11-12 20:29:39 -0300

    Botão CTA e teste no painel de rotinas

--- client/src/pages/dashboard/Rotina.jsx
+++ client/src/pages/dashboard/Rotina.jsx
@@ -1,3 +1,19 @@
+import sportBanner from '../../assets/SPORTRECIFE.webp';
+import BannerDashboard from '../../components/BannerDashboard';
+import ButtonCta from '../../components/ButtonCta';
+
 export default function Rotina() {
-    return <h1>Rotina</h1>;
+    return (
+        <div className="bg-cream-100 dark:bg-night-950 h-full w-full p-12">
+            <header>
+                <BannerDashboard banner={sportBanner}>
+                    <h1 className="text-4xl font-black 2xl:text-5xl">Crie sua rotina</h1>
+                    <span>Use nossa IA para criar sua rotina. Você pode também pode criar cards únicos.</span>
+                    <br />
+                    <br />
+                    <ButtonCta>CRIAR ROTINA</ButtonCta>
+                </BannerDashboard>
+            </header>
+        </div>
+    );
 }
