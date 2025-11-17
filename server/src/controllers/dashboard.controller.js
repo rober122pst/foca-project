ddb0205a8b7cd2e9ae79b7fd14caea48b2df3dab
rober122pst <jrobertinhor.9@gmail.com>
2025-11-17 20:14:01 -0300

    Controller do overview criada

--- server/src/controllers/dashboard.controller.js
+++ server/src/controllers/dashboard.controller.js
@@ -14,6 +14,26 @@ export async function getOverviewData(req, res) {
         const completedTasks = userTasks.filter(task => task.completed).length;
         const userRoutines = await prisma.routine.findMany({ where: { userId } });
         const routinesCount = userRoutines.length;
-        
+        const userAchievements = await prisma.userAchiviement.findMany({ where: { userId } })
+
+        return res.json({
+            stats: {
+                streak: userGamefication?.streakCurrent || 0,
+                totalTimeFocused: 93, // TODO: fazer isso aqui depois
+                completedTasks: completedTasks,
+                activeRoutines: routinesCount
+            },
+            levelProgress: {
+                level: userGamefication?.level || 1,
+                currentXp: userGamefication?.currentXp || 0,
+                nextLevelXp: userGamefication ? (userGamefication.level + 1) * 100 : 100
+            },
+            taskList: [
+                ...userTasks.slice(0, 4)
+            ],
+            achievements: userAchievements,
+        })
+    } catch (error) {
+        console.error("Erro ao buscar dados do dashboard:", error);
     }
 }
