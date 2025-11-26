a8c19b036e6b5c33672e7f5ee7a40ff4dc7b1d38
rober122pst <jrobertinhor.9@gmail.com>
2025-11-26 09:50:49 -0300

    Logout corrigido

--- server/src/auth/auth.routes.js
+++ server/src/auth/auth.routes.js
@@ -1,4 +1,4 @@
-import { login, register } from './auth.controller.js';
+import { login, logout, register } from './auth.controller.js';
 
 import express from 'express';
 import { refresh } from './refresh.controller.js';
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
-router.post('/refresh', refresh)
+router.post('/refresh', refresh);
+router.post('/logout', logout)
 
 export default router;
